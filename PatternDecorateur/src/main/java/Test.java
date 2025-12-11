03f277fbcb0bbd9bdf58c3ba9fd0f7fd8a06dc1c
RajaaKacemi <rajaa01.kacemi@gmail.com>
2025-12-11 15:04:09 +0100

    first commit

--- PatternDecorateur/src/main/java/Test.java
+++ PatternDecorateur/src/main/java/Test.java
@@ -7,15 +7,15 @@ public class Test {
     public static void main(String[] args){
         Boisson boisson;
         boisson = new Espresso();
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
+        System.out.println("Commande : " + boisson.getDescription());
+        System.out.println("Prix :" + boisson.cout());
         System.out.println("-------------");
         boisson = new Chocolat(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
+        System.out.println("Commande : " + boisson.getDescription());
+        System.out.println("Prix :" + boisson.cout());
         System.out.println("-------------");
         boisson = new Noisette(boisson);
-        System.out.println(boisson.getDescription());
-        System.out.println(boisson.cout());
+        System.out.println("Commande : " + boisson.getDescription());
+        System.out.println("Prix :" + boisson.cout());
     }
 }
